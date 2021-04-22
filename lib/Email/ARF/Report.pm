f7defaa71f3dfa473e52ee0d8596d40229dfe27f
Chase Venters <chase.venters@chaseventers.com>
2021-04-22 08:34:15 -0500

    Remove outdated comment

--- lib/Email/ARF/Report.pm
+++ lib/Email/ARF/Report.pm
@@ -186,8 +186,6 @@ sub create {
 
   my $report = Email::MIME->create(
     attributes => {
-      # It is so asinine that I need to do this!  Only certain blessed
-      # attributes are heeded, here.  The rest are dropped. -- rjbs, 2007-03-21
       content_type  => 'multipart/report',
       'report-type' => 'feedback-report',
     },
